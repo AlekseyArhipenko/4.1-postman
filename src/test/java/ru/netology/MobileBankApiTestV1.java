2f42f2d2c32cc32054b85fe8da89fe6599cd0826
AlekseyArhipenko <arhipenko1990@inbox.ru>
2022-02-23 13:29:33 +0300

    invalid deploy

---
+++ src/test/java/ru/netology/MobileBankApiTestV1.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnData() {
+        // Given - When - Then
+// Предусловия
+        String s = "[0].id";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+
+
+        ;
+
+    }
+}
